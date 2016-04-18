08047cb6c5128d3beb3e94db29c6101ee8bd1b4e
wibowo-hari <hari_wibowo156@yahoo.co.id>
2016-04-18 10:07:58 +0700

    Added files via upload

--- views/admin/layout/menu.php
+++ views/admin/layout/menu.php
@@ -25,14 +25,30 @@
                     </li>
                     <li class="ripple">
                       <a class="tree-toggle nav-header">
-                        <span class="fa-diamond fa"></span> Laporan
+                        <span class="fa-diamond fa"></span> Laporan Pegawai
                         <span class="fa-angle-right fa right-arrow text-right"></span>
                       </a>
                       <ul class="nav nav-list tree">
-                        <li><a href="#">Laporan x</a></li>
-                        <li><a href="#">Laporan y</a></li>
+                        <li><a href="<?php echo base_url(); ?>admin/Laporan_pegawai_unit/index">Unit Kerja & Satuan Kerja</a></li>
+                        <li><a href="<?php echo base_url(); ?>admin/Laporan_pegawai_diklat/index">Diklat</a></li>
+                        <li><a href="<?php echo base_url(); ?>admin/Laporan_pegawai_sertifikasi/index">Sertifikasi</a></li>
+                        <li><a href="<?php echo base_url(); ?>admin/Laporan_pegawai_talenta/index">Data Talenta</a></li>
+                        <li><a href="#">Cetak CV</a></li>
                       </ul>
                     </li>
+                    <li class='ripple'>
+                      <a class='tree-toggle nav-header'>
+                        <span class='fa-diamond fa'></span> Data Master
+                        <span class='fa-angle-right fa right-arrow text-right'></span>
+                      </a>
+                      <ul class='nav nav-list tree'>
+                        <li><a href='#'>Direktorat</a></li>                        
+                        <li><a href='#'>Jabatan</a></li>                        
+                        <li><a href='#'>Bidang</a></li>
+                        <li><a href='#'>Kompetensi</a></li>                        
+                        <li><a href='#'>Grade</a></li>                        
+                      </ul>
+                    </li>                    
                     <li class="ripple">
                       <a class="tree-toggle nav-header">
                         <span class="fa-diamond fa"></span> Panduan
