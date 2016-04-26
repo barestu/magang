029d28b9f7a65edb65e3cba4acc01da0defc9551
wibowo-hari <hari_wibowo156@yahoo.co.id>
2016-04-26 09:50:04 +0700

    Added files via upload

--- views/admin/layout/header.php
+++ views/admin/layout/header.php
@@ -24,18 +24,21 @@
                   </div>
                 </li>
               </ul>
-
+                <?php $notifikasi = $this->session->userdata('notifikasi'); ?>
               <ul class="nav navbar-nav navbar-right user-nav">
                 <li class="user-name"><span>Selamat Datang, <?php echo $username; ?></span></li>
-                  <li class="dropdown avatar-dropdown">
-                   <img src="<?php echo base_url() ?>asset/img/avatar.jpg" class="img-circle avatar" alt="user name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"/>
+                  <li class="dropdown avatar-dropdown">                                    
+                   <img src="<?php echo base_url() ?>asset/img/avatar.jpg" class="img-circle avatar" alt="user name" style="cursor:pointer" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                   <span class="badge badge-primary"><?php echo $notifikasi; ?></span> 
                    <ul class="dropdown-menu user-dropdown">
                       <li><a href="<?php echo base_url(); ?>admin/Pengaturan_akun"><span  class="fa fa-wrench "></span>  Pengaturan Akun</a></li>
                       <li><a href="<?php echo base_url(); ?>admin/Manajemen_user"><span class="fa fa-database"></span>  Manajemen User</a></li>
                       <li role="separator" class="divider"></li>
+                      <li><a href="<?php echo base_url(); ?>admin/Notifikasi_pegawai"><span class="fa fa-bell-o"></span> Notifikasi Pegawai <span class="badge badge-info"><?php echo $notifikasi; ?></span></a></li>
+                      <li role="separator" class="divider"></li>
                   </ul>
-                </li>
-                <li ><a <span class="fa fa-power-off" data-toggle="modal" data-target="#myModal"></span></a></li>
+                  </li>              
+                <li ><a <span class="fa fa-power-off" style="font-size:24px" data-toggle="modal" data-target="#myModal"></span></a></li>
               </ul>
             </div>
           </div>
