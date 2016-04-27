edb0344362b8b45625dc3c746f59ecaee594edfb
wibowo-hari <hari_wibowo156@yahoo.co.id>
2016-04-27 09:58:23 +0700

    Added files via upload

--- models/Model_user.php
+++ models/Model_user.php
@@ -1,12 +1,41 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 	class Model_user extends CI_Model {
-		
+
 		public function cek_user($data) {
 			$query = $this->db->get_where('tbl_user_login', $data);
 			return $query;
 		}
 
+	
+	public function sign_up() {
+		$query = $this->db->query("SELECT MAX(id_user_login) AS max  FROM tbl_user_login");
+	return $query; 
 	}
+
+	public function input_sign_up() {	
+			$userbaru = $this->input->post("username");
+
+ 			$this->db->select('*');
+ 			$this->db->from('tbl_user_login');
+  			$this->db->where('username', $userbaru);
+  			$query = $this->db->get();  					
+			if ($query->num_rows() < 1){
+				$input['id_user_login'] = $this->input->post("id_user_login");
+				$input['username'] = $userbaru;				
+				$input['email'] = $this->input->post("email");
+				$input['nip'] = $this->input->post("nip");
+				$input['level'] = "user";
+				$input['password'] = md5($this->input->post("password"));
+			
+				$this->db->insert("tbl_user_login",$input);
+				if($this->db->affected_rows() == 1) {
+					return TRUE; }					
+				}
+			else {
+				return FALSE;
+					}		
+	 }
+}
 
 ?>
---
+++ models/Notifikasi_pegawai_model.php
@@ -0,0 +1,21 @@
+<?php
+class Notifikasi_pegawai_model extends CI_Model {
+ public function __construct() {
+ $this->load->database();
+ }
+ 
+ // Kode untuk menampilkan data tabel pada database
+ public function kontrak_tempo() {
+
+ $query = $this->db->query("SELECT nip, nama, nama_status, akhir_kontrak, DATEDIFF(akhir_kontrak,CURDATE()) AS selisih
+			FROM tbl_pegawai  NATURAL JOIN tb_pegawai_pkwt  NATURAL JOIN tb_status 
+			WHERE tbl_pegawai.id_peg = tb_pegawai_pkwt.id_peg
+			AND tbl_pegawai.id_status = tb_status.id_status 
+			AND tb_pegawai_pkwt.akhir_kontrak 
+				BETWEEN CURDATE() 
+					AND ADDDATE(CURDATE(), INTERVAL 1 MONTH)				
+			ORDER BY akhir_kontrak DESC");
+ return $query;
+ }
+}
+?>
