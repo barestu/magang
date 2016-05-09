2a8b8046fa4bc3b63b697b2fc32b6cc86151e562
Fandy Barestu <fheartnet@gmail.com>
2016-05-09 09:18:33 +0700

    model pkwt completed
    model pkwt completed

---
+++ models/Model_data_pkwt.php
@@ -0,0 +1,21 @@
+<?php
+class Model_data_pkwt extends CI_Model {
+ public function __construct() {
+	$this->load->database();
+ }
+ 
+ // Ambil data pegawai yang ditentukan
+ public function getData($id) {
+	$this->db->select('*');
+	$this->db->from('tbl_pegawai AS tp, tb_pegawai_pkwt AS tpp, tb_jabatan AS tj, tb_bidang AS tb, tb_direktorat AS td');
+	$this->db->where('tp.id_peg', $id);
+	$this->db->where('tp.id_peg = tpp.id_peg');
+	$this->db->where('tp.id_bid = tb.id_bid');
+	$this->db->where('tp.id_jab = tj.id_jab');
+	$this->db->where('tp.id_direktorat = td.id_direktorat');
+	$query = $this->db->get();
+	
+	return $query->row_array();
+ }
+}
+?>
