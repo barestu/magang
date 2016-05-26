642a530a8bd3ff1574ea6d80c14ca4561def800c
Fandy Barestu <fheartnet@gmail.com>
2016-05-26 08:06:59 +0700

    Add files via upload

---
+++ views/admin/cv/cv_print.php
@@ -0,0 +1,82 @@
+<!-- start: Content -->
+<div class="panel invoice-v1-content" id="printableArea">
+	<div class="panel-body">
+		<div class="col-md-12">
+		  <div>
+		    <label class="col-sm-6 control-label"><h3>CURRICULUM VITAE</h3></label>                          
+		  </div></br></br>
+		</div>
+		
+		<div class="col-md-10">
+		  <table class="table-borderless">
+			<tr>
+			  <td><b>1.</b></td><td><b>Proposed Position</b></td><td>:</td>
+			  <td>Civil Engineering</td>
+			</tr>
+			<tr>
+			  <td><b>2.</b></td><td><b>Name of Firm</b></td><td>:</td>
+			  <td>PT. Prima Layanan Nasional Enjiniring</td>
+			</tr>
+			<tr>
+			  <td><b>3.</b></td><td><b>Name of Personel</b></td><td>:</td>
+			  <td>Andriyanto</td>
+			</tr>
+			<tr>
+			  <td><b>4.</b></td><td><b>Date of Birth</b></td><td>:</td>
+			  <td>October 2nd 1983</td>
+			  <td><b>Nationality</b></td><td>:</td>
+			  <td>Indonesian</td>
+			</tr>
+			<tr>
+			  <td><b>5.</b></td><td><b>Proposed Position</b></td><td>:</td>
+			  <td>Civil Engineering</td>
+			</tr>
+		  </table>
+		  
+		  <table class="table-borderless">
+			<tr>
+			  <td><b>6.</b></td><td><b>Membership of Professional Associations : </b></td>
+			</tr>
+			<tr>
+			  <td></td><td>- Insinyur Professional Pratama, certified by The Institution Of Engineers Indonesia (PII), 2010</td>
+			</tr>
+		  </table>
+		  
+		  <table class="table-borderless">
+			<tr>
+			  <td><b>7.</b></td><td><b>Proficiency Training:</b></td>
+			</tr>
+			<tr>
+			  <td></td><td>- STAD PRO 2000 Training- Warren Consulting and Construction, 2010</td>
+			</tr>
+			<tr>
+			  <td></td><td>- Financial Planning - PT Primasi Assessment, Training and Development, 2010</td>
+			</tr>
+			<tr>
+			  <td></td><td>- Personal Empowerment Training - PT Psiko Utama, 2010</td>
+			</tr>
+		  </table>
+		  
+		  <table class="table-borderless">
+			<tr>
+			  <td><b>8.</b></td><td><b>Countries of Work Experiences : </b>Indonesia</td>
+			</tr>
+		  </table>
+		  
+		  <table class="table-borderless">
+			<tr>
+			  <td><b>9.</b></td><td><b>Languages : </b></td><td>Speaking</td><td>Reading</td><td>Writing</td>
+			</tr>
+			<tr>
+			  <td></td><td>- Indonesian</td><td>Native</td><td>Native</td><td>Native</td>
+			</tr>
+			<tr>
+			  <td></td><td>- English</td><td>Very Good</td><td>Very Good</td><td>Very Good</td>
+			</tr>
+		  </table>
+		
+		</div>
+	</div>
+</div>
+</div>
+<!-- end: content -->
