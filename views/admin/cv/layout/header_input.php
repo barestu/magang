3f70ec43ef94d45d34b21a6e7801d587862a71a9
Fandy Barestu <fheartnet@gmail.com>
2016-05-26 08:07:33 +0700

    Add files via upload

---
+++ views/admin/cv/layout/header_input.php
@@ -0,0 +1,19 @@
+<!-- start: Header -->
+<body id="mimin" class="dashboard">
+
+<div class="container invoice invoice-v1">
+<nav class="navbar navbar-default header invoice-v1-tool container navbar-fixed-top">
+  <div class="col-md-12 nav-wrapper">
+	<div class="navbar-header" style="width:100%;">
+		<a href="<?php echo base_url() ?>admin/data_pegawai_pkwt?id=<?php echo $data_peg['id_peg']?>" class="navbar-brand"><span class="fa fa-chevron-left" style="font-size:28px"></span>
+		</a>
+
+	  <ul class="nav navbar-nav navbar-right user-nav">            
+		<li >
+		  <a <span class="fa fa-power-off" style="font-size:24px" data-toggle="modal" data-target="#myModal"></span></a>
+		</li>                
+	  </ul>
+	</div>
+  </div>
+</nav>
+<!-- end: Header -->
