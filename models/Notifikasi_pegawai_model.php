045205b5e34696e1cdbc96aadb3e0e3b193dca51
wibowo-hari <hari_wibowo156@yahoo.co.id>
2016-06-14 17:19:11 +0700

    Kalo notifikasi eror di NIP

--- models/Notifikasi_pegawai_model.php
+++ models/Notifikasi_pegawai_model.php
@@ -7,7 +7,7 @@ class Notifikasi_pegawai_model extends CI_Model {
  // Kode untuk menampilkan data tabel pada database
  public function kontrak_tempo() {
 
- $query = $this->db->query("SELECT id_peg, no_kontrak, nama, nama_status, berakhir_kontrak, DATEDIFF(berakhir_kontrak,CURDATE()) AS selisih
+ $query = $this->db->query("SELECT id_peg, nip, no_kontrak, nama, nama_status, berakhir_kontrak, DATEDIFF(berakhir_kontrak,CURDATE()) AS selisih
 			FROM tbl_pegawai  NATURAL JOIN tb_pegawai_pkwt  NATURAL JOIN tb_status 
 			WHERE tbl_pegawai.id_peg = tb_pegawai_pkwt.id_peg
 			AND tbl_pegawai.id_status = tb_status.id_status 
