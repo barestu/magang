fc26f79068d7feebfc72bdae6386cb3549533394
Fandy Barestu <fheartnet@gmail.com>
2016-06-14 16:33:39 +0700

    Update DB June 14 2016
    - tb_kompetensi relation added to tbl_pegawai

--- db_plne.sql
+++ db_plne.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2016 at 05:08 AM
+-- Generation Time: Jun 14, 2016 at 11:31 AM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.16
 
@@ -33,7 +33,8 @@ CREATE TABLE IF NOT EXISTS `tbl_pegawai` (
   `id_status` int(10) NOT NULL,
   `id_bid` int(10) NOT NULL,
   `id_jab` int(10) NOT NULL,
-  `id_direktorat` int(11) NOT NULL,
+  `id_direktorat` int(10) NOT NULL,
+  `id_kompetensi` int(10) NOT NULL,
   `nip` varchar(20) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `alamat` text NOT NULL,
@@ -54,11 +55,14 @@ CREATE TABLE IF NOT EXISTS `tbl_pegawai` (
 -- Dumping data for table `tbl_pegawai`
 --
 
-INSERT INTO `tbl_pegawai` (`id_peg`, `id_status`, `id_bid`, `id_jab`, `id_direktorat`, `nip`, `nama`, `alamat`, `no_telp`, `email`, `email2`, `tempat_lahir`, `tgl_lahir`, `jenis_kelamin`, `agama`, `status`, `jml_keluarga`) VALUES
-('1', 1, 1, 1, 1, '0000001 - RTZ', 'Artour Babaev', 'Cengkareng, Jakarta Barat', '081804323001', 'artour1990@gmail.com', 'artour@plne.com', 'Jakarta', '1994-12-05', 'Laki-Laki', 'Islam', 'Menikah', 0),
-('2', 1, 9, 87, 2, '0000002 - SPG', 'Jacky Lmao', 'Montreal, Canada', '081809809809', 'jacky.lmao@lmao.co', 'jacky.lmao@lmao.co', 'Kota Gede', '1991-02-09', 'Perempuan', 'Lainnya', 'Belum Menikah', 0),
-('4', 2, 15, 143, 2, '0000022 - KZL', 'Amy Reinbach', 'Cikarang', '081804232232', 'asdf@adfs.cd', 'asdf@adfs.cd', 'Asgard', '1986-10-05', 'Laki-Laki', 'Islam', 'Belum Menikah', 0),
-('5', 2, 15, 19, 2, '0000023 - MSR', 'Ramsus Filipsen', 'Jakarta', '09183219123', 'qweqwe@qwew.c', 'qeqweqw@fsd.d', 'Jakarta', '1989-02-28', 'Laki-Laki', 'Islam', 'Belum Menikah', 0);
+INSERT INTO `tbl_pegawai` (`id_peg`, `id_status`, `id_bid`, `id_jab`, `id_direktorat`, `id_kompetensi`, `nip`, `nama`, `alamat`, `no_telp`, `email`, `email2`, `tempat_lahir`, `tgl_lahir`, `jenis_kelamin`, `agama`, `status`, `jml_keluarga`) VALUES
+('1', 1, 1, 1, 1, 0, '0000001-RTZ', 'Artour Babaev', 'Cengkareng, Jakarta Barat', '081804323001', 'artour1990@gmail.com', 'artour@plne.com', 'Jakarta', '1994-12-05', 'Laki-Laki', 'Islam', 'Menikah', 1),
+('7', 1, 9, 14, 3, 0, '0000003-STR', 'Bryan Cranston', 'Jakarta', '123432123123', 'qwe@we', 'wefae@w', 'Gertadef', '1985-01-05', 'Perempuan', 'Islam', 'Belum Menikah', 0),
+('9', 2, 19, 6, 2, 0, 'KONTRAK', 'Aaron Paul', 'Fertwa', '1212121212', 'bbbbb@qqq', 'qqqqq@aaa', 'Feraste', '1988-11-02', 'Laki-Laki', 'Islam', 'Belum Menikah', 0),
+('11', 2, 1, 91, 1, 0, 'PENSIUN PLN', 'Georg Prime', 'Ferawd', '121212122112', 'qqq@qq', 'qqq@qq', 'Ferpos', '1978-09-03', 'Laki-Laki', 'Katholik', 'Menikah', 0),
+('12', 2, 19, 5, 2, 0, 'OUTSOURCING', 'Alexandr Daskovich', 'Gertyu', '121212121212', 'wqeqw@qq', 'dddd@qqq', 'Vertas', '1987-11-28', 'Laki-Laki', 'Kristen', 'Belum Menikah', 0),
+('13', 1, 4, 18, 3, 0, '11', 'asdf', 'awer', '12312', 'sdf@q', 'aweqw@qwe', 'ewr', '2014-09-29', 'Perempuan', 'Kristen', 'Belum Menikah', 1),
+('14', 2, 1, 2, 1, 0, 'PENSIUN PLN', 'Nicholas Saputra', 'Jakarta', '0002223331', 'yes@plne.com', 'ter@yes.com', 'Jakarta', '1989-03-31', 'Laki-Laki', 'Kristen', 'Belum Menikah', 0);
 
 -- --------------------------------------------------------
 
@@ -71,18 +75,41 @@ CREATE TABLE IF NOT EXISTS `tbl_user_login` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `nip` varchar(50) NOT NULL,
-  `level` enum('admin','user') NOT NULL,
+  `level` enum('Adm.super','Adm.pkwt','Adm.organik','user') NOT NULL,
+  `status_akun` int(11) NOT NULL,
+  `nip` varchar(20) NOT NULL,
   PRIMARY KEY (`id_user_login`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `tbl_user_login`
 --
 
-INSERT INTO `tbl_user_login` (`id_user_login`, `username`, `password`, `email`, `nip`, `level`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '', '', 'admin'),
-(3, 'coba2', 'a3040f90cc20fa672fe31efcae41d2db', 'abc', '2222', 'user');
+INSERT INTO `tbl_user_login` (`id_user_login`, `username`, `password`, `email`, `level`, `status_akun`, `nip`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '', 'Adm.super', 1, ''),
+(4, 'user01', 'b75705d7e35e7014521a46b532236ec3', 'qqq@qq.c', 'user', 1, '0000001-RTZ'),
+(5, 'pkwt01', '66f15e319b2a86c826c7ad37022a9041', 'abcd@abbb', 'user', 1, 'KONTRAK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_aktivitas_proyek`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_aktivitas_proyek` (
+  `id_ap` int(11) NOT NULL AUTO_INCREMENT,
+  `id_proyek` int(11) NOT NULL,
+  `id_peg` int(11) NOT NULL,
+  `nama_ap` text,
+  PRIMARY KEY (`id_ap`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `tb_aktivitas_proyek`
+--
+
+INSERT INTO `tb_aktivitas_proyek` (`id_ap`, `id_proyek`, `id_peg`, `nama_ap`) VALUES
+(2, 12, 9, 'Review design proposed for the construction of PLTU Parit Baru based on the Contract Document');
 
 -- --------------------------------------------------------
 
@@ -124,6 +151,47 @@ INSERT INTO `tb_bidang` (`id_bid`, `nama_bid`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_cv`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_cv` (
+  `id_cv` int(11) NOT NULL AUTO_INCREMENT,
+  `id_peg` int(11) NOT NULL,
+  `nama_perusahaan` varchar(50) NOT NULL,
+  `member_asosiasi` varchar(100) NOT NULL,
+  `tahun_member` varchar(22) NOT NULL,
+  `warga_negara` varchar(22) NOT NULL,
+  `pendidikan` varchar(50) NOT NULL,
+  `negara_bekerja` varchar(22) NOT NULL,
+  `bahasa1` varchar(22) NOT NULL,
+  `bahasa2` varchar(22) NOT NULL,
+  `bahasa3` varchar(22) DEFAULT NULL,
+  `speaking1` varchar(22) NOT NULL,
+  `speaking2` varchar(22) NOT NULL,
+  `speaking3` varchar(22) DEFAULT NULL,
+  `reading1` varchar(22) NOT NULL,
+  `reading2` varchar(22) NOT NULL,
+  `reading3` varchar(22) DEFAULT NULL,
+  `writing1` varchar(22) NOT NULL,
+  `writing2` varchar(22) NOT NULL,
+  `writing3` varchar(22) DEFAULT NULL,
+  `record_dari` varchar(22) NOT NULL,
+  `record_sampai` varchar(22) NOT NULL,
+  `record_perusahaan` varchar(50) NOT NULL,
+  `record_jabatan` varchar(22) NOT NULL,
+  PRIMARY KEY (`id_cv`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tb_cv`
+--
+
+INSERT INTO `tb_cv` (`id_cv`, `id_peg`, `nama_perusahaan`, `member_asosiasi`, `tahun_member`, `warga_negara`, `pendidikan`, `negara_bekerja`, `bahasa1`, `bahasa2`, `bahasa3`, `speaking1`, `speaking2`, `speaking3`, `reading1`, `reading2`, `reading3`, `writing1`, `writing2`, `writing3`, `record_dari`, `record_sampai`, `record_perusahaan`, `record_jabatan`) VALUES
+(1, 9, 'PT. PLN Enjiniring', 'Association of The Grand Maester', '2010', 'Indonesian', 'Bachelor Degree of Civil Engineering', 'Indonesia', 'Indonesian', 'English', '', 'Native', 'Very Good', '', 'Native', 'Very Good', '', 'Native', 'Very Good', '', '2008', 'present', 'PT. PLN Enjiniring', 'Civil Engineer');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_diklat`
 --
 
@@ -143,7 +211,8 @@ CREATE TABLE IF NOT EXISTS `tb_diklat` (
 INSERT INTO `tb_diklat` (`id_dik`, `id_peg`, `nama_dik`, `jenis_dik`, `tahun_dik`) VALUES
 (7, 1, 'DIKLAT BABY RAGE', 'Diklat Profesi', '2007'),
 (8, 1, 'DIKLAT NA DOTO BEST DOTO', 'Diklat Profesi', '2013'),
-(10, 4, 'DIKLAT FOR BEGINNER', 'Diklat Profesi', '2009');
+(9, 9, 'Personal Empowerment Training', 'Diklat Profesi', '2005'),
+(10, 9, 'Geotechnical Problems in Indonesia - Plaxis', 'Diklat Profesi', '2014');
 
 -- --------------------------------------------------------
 
@@ -373,7 +442,7 @@ CREATE TABLE IF NOT EXISTS `tb_keluarga` (
   `tgl_kel` date NOT NULL,
   `ket_kel` varchar(20) NOT NULL,
   PRIMARY KEY (`id_kel`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `tb_keluarga`
@@ -381,8 +450,7 @@ CREATE TABLE IF NOT EXISTS `tb_keluarga` (
 
 INSERT INTO `tb_keluarga` (`id_kel`, `id_peg`, `nama_kel`, `hub_kel`, `jk_kel`, `tgl_kel`, `ket_kel`) VALUES
 (3, 1, 'Jacky Lmao', 'Istri', 'Perempuan', '1989-01-05', 'Ditanggung'),
-(6, 1, 'Matthew Bailey', 'Anak', 'Laki-laki', '1994-10-31', 'Tidak Ditanggung'),
-(7, 4, 'Roman Fominok Ale', 'Suami', 'Laki-laki', '1989-11-29', 'Ditanggung');
+(6, 1, 'Matthew Bailey', 'Anak', 'Laki-laki', '1994-10-31', 'Tidak Ditanggung');
 
 -- --------------------------------------------------------
 
@@ -474,7 +542,8 @@ CREATE TABLE IF NOT EXISTS `tb_pegawai_organik` (
 
 INSERT INTO `tb_pegawai_organik` (`id_peg`, `tgl_masuk`, `tgl_angkat`) VALUES
 (1, '2008-01-05', '2008-01-10'),
-(2, '2004-08-27', '2006-08-28');
+(7, '2006-05-04', '2006-05-05'),
+(13, '2014-10-29', '2014-10-28');
 
 -- --------------------------------------------------------
 
@@ -501,8 +570,10 @@ CREATE TABLE IF NOT EXISTS `tb_pegawai_pkwt` (
 --
 
 INSERT INTO `tb_pegawai_pkwt` (`id_peg`, `no_npwp`, `no_kontrak`, `mulai_kontrak`, `berakhir_kontrak`, `lama_kontrak`, `uang_tugas`, `uang_insentif`, `uang_makan`, `uang_transport`) VALUES
-(4, '002', '002', '2011-08-28', '2012-04-27', 8, 100000, 200000, 300000, 500000),
-(5, '003', '003', '2015-08-02', '2016-05-29', 9, 100000, 200000, 300000, 400000);
+(9, '1', '1', '2011-03-27', '2016-07-02', 64, 9000000, 9000000, 800000, 200000),
+(11, '3', '3', '2014-01-04', '2018-11-29', 58, 19000000, 9000000, 900000, 200000),
+(12, '4', '4', '2012-10-29', '2016-05-31', 43, 5000000, 5000000, 600000, 400000),
+(14, '12', '12', '2014-10-29', '2016-10-29', 24, 2000000, 3000000, 1000000, 1000000);
 
 -- --------------------------------------------------------
 
@@ -548,6 +619,34 @@ CREATE TABLE IF NOT EXISTS `tb_pensiun` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_proyek`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_proyek` (
+  `id_proyek` int(11) NOT NULL,
+  `id_peg` int(11) NOT NULL,
+  `nama_proyek` varchar(100) NOT NULL,
+  `mulai_proyek` varchar(22) NOT NULL,
+  `berakhir_proyek` varchar(22) NOT NULL,
+  `lokasi_proyek` varchar(50) NOT NULL,
+  `nama_lokasi_proyek` varchar(20) NOT NULL,
+  `pemilik_proyek` varchar(50) NOT NULL,
+  `fitur_proyek` varchar(50) NOT NULL,
+  `jabatan_proyek` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_proyek`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_proyek`
+--
+
+INSERT INTO `tb_proyek` (`id_proyek`, `id_peg`, `nama_proyek`, `mulai_proyek`, `berakhir_proyek`, `lokasi_proyek`, `nama_lokasi_proyek`, `pemilik_proyek`, `fitur_proyek`, `jabatan_proyek`) VALUES
+(2, 9, 'PLTU KALTENG 2x60 MW (1st Stage CFSPP Crashed Programme)', '2000', '2006', 'Pulau Pisau, Central Kalimantan', 'PLTU KALTENG', 'PT PLN (Persero)', 'Engineering Supervision', 'Lead Civil Engineer'),
+(12, 9, 'PLTU Parit Baru 2x50', '2005', '2010', 'Parit, Kalimantan', 'PLTU Parit', 'PT PLN (Persero)', '', 'Lead Civil Engineer');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_sertifikasi`
 --
 
@@ -555,7 +654,7 @@ CREATE TABLE IF NOT EXISTS `tb_sertifikasi` (
   `id_ser` int(10) NOT NULL AUTO_INCREMENT,
   `id_peg` int(10) NOT NULL,
   `tanggal_ser` date NOT NULL,
-  `nama_ser` varchar(50) NOT NULL,
+  `nama_ser` varchar(100) NOT NULL,
   PRIMARY KEY (`id_ser`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
@@ -567,7 +666,7 @@ INSERT INTO `tb_sertifikasi` (`id_ser`, `id_peg`, `tanggal_ser`, `nama_ser`) VAL
 (2, 1, '2011-02-05', 'SERTIFIKASI OF THE MEMELORD'),
 (4, 1, '2009-03-30', 'SERTIFIKASI 322 YOLO'),
 (6, 1, '2009-10-05', 'SERTIFIKASI BABYRAEG '),
-(7, 4, '2013-01-31', 'SERTIFIKASI PEGAWAI 1');
+(7, 9, '2010-10-28', 'Insinyur Professional Pratama, certified by The Grand Maester');
 
 -- --------------------------------------------------------
 
@@ -613,6 +712,31 @@ CREATE TABLE IF NOT EXISTS `tb_talenta` (
 INSERT INTO `tb_talenta` (`id_talenta`, `id_peg`, `mulai_tanggal`, `berakhir_tanggal`, `semester`, `id_grade`, `poin`) VALUES
 (1, 1, '2011-07-01', '2011-12-31', '1', 3, 'A'),
 (2, 1, '2012-01-01', '2012-06-30', '2', 3, 'B');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_upload_gambar`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_upload_gambar` (
+  `id_gambar` int(11) NOT NULL AUTO_INCREMENT,
+  `id_peg` int(11) NOT NULL,
+  `nama_gambar` varchar(35) NOT NULL,
+  `tipe_gambar` varchar(10) NOT NULL,
+  `status_gambar` enum('default','custom') NOT NULL,
+  PRIMARY KEY (`id_gambar`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `tb_upload_gambar`
+--
+
+INSERT INTO `tb_upload_gambar` (`id_gambar`, `id_peg`, `nama_gambar`, `tipe_gambar`, `status_gambar`) VALUES
+(1, 13, 'avatar.jpg', 'image/jpg', 'default'),
+(2, 1, 'avatar.png', 'image/png', 'custom'),
+(3, 9, 'avatar_1465888441.jpg', 'image/jpeg', 'custom'),
+(4, 14, 'avatar.jpg', 'image/jpg', 'default');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
